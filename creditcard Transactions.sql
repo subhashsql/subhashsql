24958fc9d478ccdc89834cbac7d4f0f9f63bcf18
subhashsql <139942267+subhashsql@users.noreply.github.com>
2023-08-31 10:39:14 +0530

    Add files via upload
    Credit card transactions data and some querries using case statement

---
+++ creditcard Transactions.sql
@@ -0,0 +1,36 @@
+use creditcardtransactions
+go
+
+--------------- to check the column names and data
+select top 5 *
+from dbo.credit_card_transcations
+go
+
+----------- to check the type of card available
+
+select distinct card_type
+from dbo.credit_card_transcations
+go
+
+------------------
+
+select 
+	sum(case when card_type='Gold' then amount   end) as 'Gold card transaction Total'
+from dbo.credit_card_transcations
+go
+
+select  distinct city
+		,sum(amount) as'tot'
+from dbo.credit_card_transcations
+group by city
+having sum(amount) > 10000000
+go
+
+---------------
+select sum( case when card_type='Gold' then amount end) as 'Goldcardtransactions Total',
+	   sum( case when card_type='Platinum' then amount end) as 'Platinum cardtransactions Total',
+	   sum( case when card_type='Silver' then amount end) as 'Silver cardtransactions Total',
+	   sum( case when card_type='Signature' then amount end) as 'Signature cardtransactions Total'
+
+from dbo.credit_card_transcations
+go
